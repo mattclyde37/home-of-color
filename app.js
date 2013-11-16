8503a7e20dcbd870cfc9460e2f4cc14b00f14325
Matt Clyde <matt@mdsc.com>
2013-11-16 11:28:45 -0700

    Initial commit.  Setup project structure

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+app.post("/ChangeColor", function (req, res){
+	console.log(req.body);
+	res.send("ok");
+});
+
+app.post("/SetIp/:token", function (req, res){
+	if (req.params.token == "myToken")
+	{
+
+	}
+});
