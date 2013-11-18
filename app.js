3417a3a1df634ccc5cb0b7e33872a74719a1e4fe
Matt Clyde <matt@mdsc.com>
2013-11-18 14:15:23 -0700

    Trying port 80

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
