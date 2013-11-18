bc74431962eb6f49fe3024fe0b2c21c6d25b31b0
Matt Clyde <matt@mdsc.com>
2013-11-17 18:08:21 -0700

    Trying simple project deploy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+ /*
+
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -43,5 +45,20 @@ app.post("/SetIp/:token", function (req, res){
 	{
 		// set known ip address...
 	}
+});
+
+*/
+
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
 
