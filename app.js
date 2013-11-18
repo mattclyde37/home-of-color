55f6e2c33ea374ab5996342ba0244b963adaf54c
Matt Clyde <matt@mdsc.com>
2013-11-18 14:10:43 -0700

    Change procfile back to app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
  * Module dependencies.
  */
 
- /*
-
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -46,19 +44,3 @@ app.post("/SetIp/:token", function (req, res){
 		// set known ip address...
 	}
 });
-
-*/
-
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
